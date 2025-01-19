07d2dc685fd64fbc661d4417786393535a946ab5
Adil <a@a.com>
2025-01-19 23:25:04 +0100

    User, post, comment models

---
+++ app/Models/Comment.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+    /**
+     * str[400] content
+     * foreignId user_id
+     * foreignId post_id
+     */
+    use HasFactory;
+    
+    protected $fillable = ['content', 'user_id', 'post_id'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
