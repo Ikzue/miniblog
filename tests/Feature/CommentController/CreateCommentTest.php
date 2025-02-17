f289fc59ab6e6a1a47cab75abe5b2afa374747a5
Ikzue <adil-75@live.fr>
2025-02-18 00:31:25 +0100

    miniblog - test - create

--- tests/Feature/CommentController/CreateCommentTest.php
+++ tests/Feature/CommentController/CreateCommentTest.php
@@ -6,33 +6,82 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 use App\Models\Comment;
+use App\Models\User;
+use App\Models\Post;
 
 class CreateCommentTest extends TestCase
 {
     use RefreshDatabase;
 
+    private function authUser()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        return $user;
+    }
+
     public function test_no_auth(): void
     {
-        $this->markTestIncomplete("Check POST 'comments.store' redirect");
+        $post = Post::factory()->for(User::factory()->create())->create(); 
+
+        $response = $this->post('/api/comments', [
+            'content' => 'My content',
+            'post_id' => $post->id,
+        ]);
+        $response->assertRedirectToRoute('login');
     }
 
     public function test_auth(): void
     {
-        $this->markTestIncomplete();
+        $user = $this->authUser();
+        $post = Post::factory()->for($user)->create(); 
+
+        $response = $this->post('/api/comments', [
+            'content' => 'My content',
+            'post_id' => $post->id,
+        ]);
+        $response->assertStatus(201);
     }
 
     public function test_create_OK(): void
     {
-        $this->markTestIncomplete("Assert new comment fields and number of comments in db");
+        $user = $this->authUser();
+        $post = Post::factory()->for($user)->create(); 
+        $reqData = [
+            'content' => 'My content',
+            'post_id' => $post->id,
+        ];
+        $this->assertDatabaseCount('comments', 0);
+
+        $response = $this->post('/api/comments', $reqData);
+        $response->assertCreated();
+
+        $this->assertDatabaseCount('comments', 1);
+        $this->assertDatabaseHas('comments', [
+            'content' => $reqData['content'],
+            'post_id' => $post->id,
+            'user_id' => $user->id,
+        ]);
     }
 
     public function test_create_missing_field_KO(): void
     {
-        $this->markTestIncomplete();
+        $this->authUser();
+        $this->assertDatabaseCount('comments', 0);
+
+        $response = $this->post('/api/comments', [
+            'content' => 'My content',
+        ]);
+        $response->assertInvalid(['post_id']);
+
+        $this->assertDatabaseCount('comments', 0);
     }
 
     public function test_get_disabled(): void
     {
-        $this->markTestIncomplete("Check GET 'comments.create' is disabled");
+        $this->authUser();
+
+        $response = $this->get('/api/comments/create');
+        $response->assertStatus(404);
     }
 }
--- tests/Feature/PostController/CreatePostTest.php
+++ tests/Feature/PostController/CreatePostTest.php
@@ -5,35 +5,77 @@ namespace Tests\Feature\PostController;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-use App\Models\Post;
 use App\Models\User;
 
 class CreatePostTest extends TestCase
 {
     use RefreshDatabase;
 
+    private function authUser()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        return $user;
+    }
+
     public function test_no_auth(): void
     {
-        $this->markTestIncomplete("Check POST 'posts.store' redirect");
+        $response = $this->post('/api/posts', [
+            'title' => 'My title',
+            'content' => 'My content',
+        ]);
+        $response->assertRedirectToRoute('login');
     }
 
     public function test_auth(): void
     {
-        $this->markTestIncomplete();
+        $this->authUser();
+
+        $response = $this->post('/api/posts', [
+            'title' => 'My title',
+            'content' => 'My content',
+        ]);
+        $response->assertRedirectToRoute('posts.list.ui');
     }
 
     public function test_create_OK(): void
     {
-        $this->markTestIncomplete("Assert new post fields and number of posts in db");
+        $user = $this->authUser();
+        $reqData = [
+            'title' => 'My title',
+            'content' => 'My content',
+        ];
+        $this->assertDatabaseCount('posts', 0);
+
+        $response = $this->post('/api/posts', $reqData);
+        $response->assertSessionHas('success', 'Post created successfully');
+        $response->assertRedirect('/posts/list');
+
+        $this->assertDatabaseCount('posts', 1);
+        $this->assertDatabaseHas('posts', [
+            'title' => $reqData['title'],
+            'content' => $reqData['content'],
+            'user_id' => $user->id
+        ]);
     }
 
     public function test_create_missing_field_KO(): void
     {
-        $this->markTestIncomplete();
+        $this->authUser();
+        $this->assertDatabaseCount('posts', 0);
+
+        $response = $this->post('/api/posts', [
+            'title' => 'My title',
+        ]);
+        $response->assertInvalid(['content']);
+
+        $this->assertDatabaseCount('posts', 0);
     }
 
     public function test_get_disabled(): void
     {
-        $this->markTestIncomplete("Check GET 'posts.create' is disabled");
+        $this->authUser();
+        $response = $this->get('/api/posts/create');
+        $response->assertStatus(404);
     }
 }
